c8255f48f868726ae024129f5a8e66810ae9a1a4
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-08 03:09:19 +0530

    added timer functionality

--- app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
@@ -2,10 +2,14 @@ package com.example.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.example.a7minuteworkout.databinding.ActivityExcerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityExcerciseBinding
+    private lateinit var binding: ActivityExcerciseBinding
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExcerciseBinding.inflate(layoutInflater)
@@ -13,11 +17,36 @@ class ExerciseActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbarExercise)
 
 
-        if(supportActionBar != null){
+        if (supportActionBar != null) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
-        binding.toolbarExercise.setNavigationOnClickListener{
+        binding.toolbarExercise.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+        setRestProgressBar()
     }
+
+    private fun setRestProgressBar() {
+        binding.progressBar?.progress = restProgress;
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(p0: Long) {
+                restProgress++;
+                binding.progressBar.progress = 10 - restProgress;
+                binding.tvTimer.text = (10 - restProgress).toString();
+            }
+
+            override fun onFinish() {
+                Toast
+                    .makeText(this@ExerciseActivity, "Starting the exercise", Toast.LENGTH_LONG)
+                    .show()
+            }
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        restTimer?.cancel()
+        restProgress = 0;
+    }
+
 }
