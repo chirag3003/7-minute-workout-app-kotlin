b37114bb83ceb7d6d43943ce99614380fdb86666
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-17 11:15:30 +0530

    added text to speech for exercise name

--- app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/ExerciseActivity.kt
@@ -3,23 +3,30 @@ package com.example.a7minuteworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.view.View
 import android.widget.Toast
 import com.example.a7minuteworkout.databinding.ActivityExcerciseBinding
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private lateinit var binding: ActivityExcerciseBinding
+
+    private lateinit var ttl:TextToSpeech
+
     private var timer: CountDownTimer? = null
     private var restProgress = 0;
     private var exerciseProgress = 0;
 
     private lateinit var exerciseList: ArrayList<ExerciseModal>
-    private var currentExercisePosition = -1;
+    private var currentExercisePosition = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExcerciseBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbarExercise)
 
+        //setting text to speech
+        ttl = TextToSpeech(this,this)
 
         if (supportActionBar != null) {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -36,6 +43,11 @@ class ExerciseActivity : AppCompatActivity() {
 
     private fun setExerciseProgressBar() {
         binding.progressBar.progress = exerciseProgress
+        val exercise = exerciseList[currentExercisePosition]
+        binding.tvTitle.text = "Exercise: ${exercise.getName()}"
+        binding.exerciseImage.setImageResource(exercise.getImage())
+        binding.exerciseImage.visibility = View.VISIBLE
+        ttl.speak("${exercise.getName()}, for 30 seconds",TextToSpeech.QUEUE_FLUSH,null,"")
         timer = object : CountDownTimer(30 * 1000, 1000) {
             override fun onTick(p0: Long) {
                 exerciseProgress++;
@@ -44,7 +56,10 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
+                if(currentExercisePosition == exerciseList.size -1)
+                    return;
                 exerciseProgress = 0;
+                currentExercisePosition++;
                 setRestProgressBar()
             }
         }.start()
@@ -53,6 +68,9 @@ class ExerciseActivity : AppCompatActivity() {
 
     private fun setRestProgressBar() {
         binding.progressBar.progress = restProgress;
+        val exercise = exerciseList[currentExercisePosition]
+        binding.tvTitle.text = "Get Ready For: ${exercise.getName()}"
+        binding.exerciseImage.visibility = View.INVISIBLE
         timer = object : CountDownTimer(10000, 1000) {
             override fun onTick(p0: Long) {
                 restProgress++;
@@ -62,7 +80,6 @@ class ExerciseActivity : AppCompatActivity() {
 
             override fun onFinish() {
                 restProgress = 0;
-                currentExercisePosition++;
                 setExerciseProgressBar()
             }
         }.start()
@@ -74,6 +91,12 @@ class ExerciseActivity : AppCompatActivity() {
         restProgress = 0;
         exerciseProgress = 0;
         timer = null
+        ttl.stop()
+        ttl.shutdown()
     }
+
+    override fun onInit(p0: Int) {
+    }
+
 
 }
